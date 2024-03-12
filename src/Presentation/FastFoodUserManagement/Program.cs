3ca82bcce10c73322014b6d803668db3b87b1d4c
Fernando Melim <fernando.lief@hotmail.com>
2024-03-11 21:01:53 -0300

    Remvoendo comentarios

--- src/Presentation/FastFoodUserManagement/Program.cs
+++ src/Presentation/FastFoodUserManagement/Program.cs
@@ -4,11 +4,8 @@ using FastFoodUserManagement.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
-// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(DatabaseSettings.KeyName));
